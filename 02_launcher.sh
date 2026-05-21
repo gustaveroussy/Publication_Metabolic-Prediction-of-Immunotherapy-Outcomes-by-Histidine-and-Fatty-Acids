b3e087ea4623d76cb52d4a31accc408196c6b394
mAGLAVE <marine.aglave@gustaveroussy.fr>
2026-05-21 12:57:25 +0200

    correct typo in report + add def file for report simg

--- 02_launcher.sh
+++ 02_launcher.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 ########################################################################
-## using: sbatch /mnt/beegfs01/scratch/bioinfo_core/B26026_LAZI_01/script/01_launcher.sh
+## using: sbatch /mnt/beegfs01/scratch/bioinfo_core/B26026_LAZI_01/script/02_launcher.sh
 ########################################################################
 
 #SBATCH --job-name=B26026_LAZI_01
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=10
-#SBATCH --mem=30G
-#SBATCH --partition=longq
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=1G
+#SBATCH --partition=shortq
 
 module load singularity/3.10.5
 
-singularity exec --no-home -B /mnt/beegfs01/scratch/bioinfo_core/B26026_LAZI_01/ \
-/mnt/beegfs01/scratch/bioinfo_core/B26026_LAZI_01/script/Publication_Metabolic-Prediction-of-Immunotherapy-Outcomes-by-Histidine-and-Fatty-Acids/R_SuissaD_2026.simg \
-Rscript /mnt/beegfs01/scratch/bioinfo_core/B26026_LAZI_01/script/Publication_Metabolic-Prediction-of-Immunotherapy-Outcomes-by-Histidine-and-Fatty-Acids/01_main_workflow_reviewed.R
+mkdir -p /mnt/beegfs01/scratch/bioinfo_core/B26026_LAZI_01/data_output/report/
 
-echo "Finish!"
+singularity exec --contain -B /mnt/beegfs01/scratch/bioinfo_core/B26026_LAZI_01/ \
+/mnt/beegfs01/scratch/bioinfo_core/B26026_LAZI_01/script/Publication_Metabolic-Prediction-of-Immunotherapy-Outcomes-by-Histidine-and-Fatty-Acids/Rapport_Rmd.simg \
+Rscript -e 'rmarkdown::render("/mnt/beegfs01/scratch/bioinfo_core/B26026_LAZI_01/script/Publication_Metabolic-Prediction-of-Immunotherapy-Outcomes-by-Histidine-and-Fatty-Acids/02_report.Rmd", output_file = "/mnt/beegfs01/scratch/bioinfo_core/B26026_LAZI_01/data_output/report/B26026_LAZI_01_report.html")'
